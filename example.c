e3bee563f59a33d315e7895570dd18fecb627e69
Arthur Paulino <arthurleonardo.ap@gmail.com>
2017-05-13 18:55:20 -0300

    fix

--- example.c
+++ example.c
@@ -14,11 +14,14 @@ void printArray(int *base, int num) {
 }
 
 int main() {
-	int num = 10;
-	int base[num];
-	base[0] = 3; base[1] = 10; base[2] = 9; base[3] = 2; base[4] = 7;
-	base[5] = 1; base[6] = 4; base[7] = 8; base[8] = 0; base[9] = 6;
 	
+	int base[1024], i;
+	int num = 10;
+	
+	for (i = 0; i < num; i++) {
+		base[i] = 5*num - 3*i;
+	}
+
 	printf("Original array:\n\t"); printArray(base, num);
 	
 	heapify(base, num, sizeof(int), compar);
@@ -37,6 +40,6 @@ int main() {
 	heapsort(base, num, sizeof(int), compar);
 	
 	printf("After heapsort:\n\t"); printArray(base, num);
-	
+
 	return 0;
 }
